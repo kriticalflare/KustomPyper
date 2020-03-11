9f7e6eff4c700f7d3cc502ce3a2e98d1581a1721
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2020-03-11 19:12:34 +0530

    Set main window title

--- gui.py
+++ gui.py
@@ -63,7 +63,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "KustomPyper"))
         self.photo.setText(_translate("MainWindow", "Set a random wallpaper from reddit!"))
         self.wallpaperButton.setText(_translate("MainWindow", "Set as wallpaper"))
         self.nextWallButton.setText(_translate("MainWindow", "Next wallpaper"))
