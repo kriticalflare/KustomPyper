16f6065a784174f94e61ea30b1247babc50eb71e
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2020-03-12 20:18:11 +0530

    Persist wallpaper across reboots

--- wall.py
+++ wall.py
@@ -7,8 +7,12 @@ def changeBG(image):
     # print(directory)
     image_path = directory + "\\" + image
     # print(image_path)  
+   
+    # Constant for setting the desktop wallpaper
     SPI_SETDESKWALLPAPER = 20
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path , 0)
+    # Constant for making the wallpaper persist across reboots
+    SPIF_UPDATEINIFILE = 1
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path , SPIF_UPDATEINIFILE)
     return
 
 if __name__ == "__main__":
