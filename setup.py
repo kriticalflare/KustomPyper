fbabeb0e8e863d47a76f30c1adec8ea2612206d9
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2020-03-14 18:08:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from cx_Freeze import setup, Executable 
 
 setup(name = "KustomPyper" , 
-	version = "0.1" , 
+	version = "1.0" , 
 	description = "Find and set random wallpapers from reddit as your desktop wallpaper" , 
-	executables = [Executable("gui.py", shortcutName='Pywall', shortcutDir= "DesktopFolder")]) 
+	executables = [Executable("app.py", shortcutName='KustomPyper', shortcutDir= "DesktopFolder")]) 
